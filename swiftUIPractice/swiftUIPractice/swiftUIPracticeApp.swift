fd6e03ce803c3da3322ca2ca87c1eb3796cd94a5
Carolina Calderon <carolina.calderon@bayteq.com>
2024-07-28 17:54:50 -0500

    init

---
+++ swiftUIPractice/swiftUIPractice/swiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIPracticeApp.swift
+//  swiftUIPractice
+//
+//  Created by Carolina Calderon on 28/7/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
